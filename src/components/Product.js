b4c650ddebf34806059ad4640678bc4a9195c1d2
Herman Zhu <zhuherma@amazon.com>
2018-03-01 18:33:34 -0800

    Modified Products to be in grid row

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,17 +5,10 @@ import Image from 'react-bootstrap/lib/Image'
 
 const Product = ({ color }) => (
   <ListGroup>
-    <ListGroupItem><Image src="../../assets/bootstrap-img-mock.png" /></ListGroupItem>
+    <ListGroupItem><Image src="../../assets/bootstrap-img-mock.png" responsive /></ListGroupItem>
     <ListGroupItem>$100</ListGroupItem>
     <ListGroupItem>{color}</ListGroupItem>
   </ListGroup>
-  /*
-  <div>
-    <p>Price: $100</p>
-    <p>Skin Color: {color}</p>
-    <p>Concealer: tem</p> 
-  </div>
-  */
 )
 
 export default Product
--- src/components/Table.js
+++ src/components/Table.js
@@ -2,18 +2,23 @@ import React from 'react'
 import Product from './Product'
 import ListGroup from 'react-bootstrap/lib/ListGroup'
 import ListGroupItem from 'react-bootstrap/lib/ListGroupItem'
+import Grid from 'react-bootstrap/lib/Grid'
+import Row from 'react-bootstrap/lib/Row'
+import Col from 'react-bootstrap/lib/Col'
 
 const Table = ({ colors }) => (
   <div>
-    <ul>
-      <li>
+    <Grid>
+      <Row className="show-grid">
+        <Col xs={12} md={2}>
         <ListGroup>
           <ListGroupItem>Price</ListGroupItem>
           <ListGroupItem>Color</ListGroupItem>
         </ListGroup>
-      </li>
-      { colors.map( e => <li><Product color={ e }/></li> )}
-    </ul>
+        </Col>
+      { colors.map( e => <Product color={ e }/> ).map( e => <Col xs={12} md={2}>{e}</Col>) }
+    </Row>
+    </Grid>
   </div>
 )
 
