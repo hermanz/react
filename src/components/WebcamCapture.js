6545ed96a4c267b33a8a969cb0e2409b86cf94a9
Chaitanya Nalla <nallacha@amazon.com>
2018-03-02 13:23:00 -0800

    web cam capture logic

---
+++ src/components/WebcamCapture.js
@@ -0,0 +1,24 @@
+class WebcamCapture extends React.Component {
+  setRef = (webcam) => {
+    this.webcam = webcam;
+  }
+ 
+  capture = () => {
+    const imageSrc = this.webcam.getScreenshot();
+  };
+ 
+  render() {
+    return (
+      <div>
+        <Webcam
+          audio={false}
+          height={350}
+          ref={this.setRef}
+          screenshotFormat="image/jpeg"
+          width={350}
+        />
+        <button onClick={this.capture}>Capture photo</button>
+      </div>
+    );
+  }
+}
