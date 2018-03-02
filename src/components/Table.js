9296a69df8e93cb0dacda6a767538f66779ea711
Herman Zhu <zhuherma@amazon.com>
2018-03-01 18:47:57 -0800

    Added hidden image in attribute header

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,6 +5,7 @@ import ListGroupItem from 'react-bootstrap/lib/ListGroupItem'
 import Grid from 'react-bootstrap/lib/Grid'
 import Row from 'react-bootstrap/lib/Row'
 import Col from 'react-bootstrap/lib/Col'
+import Image from 'react-bootstrap/lib/Image'
 
 const Table = ({ colors }) => (
   <div>
@@ -12,6 +13,7 @@ const Table = ({ colors }) => (
       <Row className="show-grid">
         <Col xs={12} md={2}>
         <ListGroup>
+          <ListGroupItem><Image style={{visibility:'hidden'}} src="../../assets/bootstrap-img-mock.png" responsive /></ListGroupItem>
           <ListGroupItem>Price</ListGroupItem>
           <ListGroupItem>Color</ListGroupItem>
         </ListGroup>
