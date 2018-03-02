b5d9565bbfbda0857d2b10c673efcd38fd435262
Herman Zhu <zhuherma@amazon.com>
2018-03-01 18:07:42 -0800

    Added image elements to Products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ListGroup from 'react-bootstrap/lib/ListGroup'
 import ListGroupItem from 'react-bootstrap/lib/ListGroupItem'
+import Image from 'react-bootstrap/lib/Image'
 
 const Product = ({ color }) => (
   <ListGroup>
+    <ListGroupItem><Image src="../../assets/bootstrap-img-mock.png" /></ListGroupItem>
     <ListGroupItem>$100</ListGroupItem>
     <ListGroupItem>{color}</ListGroupItem>
   </ListGroup>
